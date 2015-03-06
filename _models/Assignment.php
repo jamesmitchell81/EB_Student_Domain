9b58433cad70bb3a8b70f181e203db4b04901a9d
James Mitchell <james.mitchell81@live.co.uk>
2015-03-06 20:10:03 +0000

    Added some model dao

---
+++ _models/Assignment.php
@@ -0,0 +1,92 @@
+<?php namespace Models {
+
+
+    class Assignment {
+        private $title = "";
+        private $release_date = "";
+        private $due_date = "";
+        private $lecturer;
+        private $module;
+
+        /**
+         * @return string
+         */
+        public function getTitle()
+        {
+            return $this->title;
+        }
+
+        /**
+         * @param string $title
+         */
+        public function setTitle($title)
+        {
+            $this->title = $title;
+        }
+
+        /**
+         * @return string
+         */
+        public function getReleaseDate()
+        {
+            return $this->release_date;
+        }
+
+        /**
+         * @param string $release_date
+         */
+        public function setReleaseDate($release_date)
+        {
+            $this->release_date = $release_date;
+        }
+
+        /**
+         * @return string
+         */
+        public function getDueDate()
+        {
+            return $this->due_date;
+        }
+
+        /**
+         * @param string $due_date
+         */
+        public function setDueDate($due_date)
+        {
+            $this->due_date = $due_date;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getLecturer()
+        {
+            return $this->lecturer;
+        }
+
+        /**
+         * @param mixed $lecturer
+         */
+        public function setLecturer(Lecture $lecturer)
+        {
+            $this->lecturer = $lecturer;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getModule()
+        {
+            return $this->module;
+        }
+
+        /**
+         * @param mixed $module
+         */
+        public function setModule(Module $module)
+        {
+            $this->module = $module;
+        }
+
+    }
+}
---
+++ _models/Module.php
@@ -0,0 +1,59 @@
+<?php namespace Models {
+
+
+    class Module {
+        private $title = "";
+        private $description = "";
+        private $level = "";
+        // lectures...?
+
+        /**
+         * @return string
+         */
+        public function getTitle()
+        {
+            return $this->title;
+        }
+
+        /**
+         * @param string $title
+         */
+        public function setTitle($title)
+        {
+            $this->title = $title;
+        }
+
+        /**
+         * @return string
+         */
+        public function getDescription()
+        {
+            return $this->description;
+        }
+
+        /**
+         * @param string $description
+         */
+        public function setDescription($description)
+        {
+            $this->description = $description;
+        }
+
+        /**
+         * @return string
+         */
+        public function getLevel()
+        {
+            return $this->level;
+        }
+
+        /**
+         * @param string $level
+         */
+        public function setLevel($level)
+        {
+            $this->level = $level;
+        }
+
+    }
+}
