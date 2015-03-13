b7aa65b6dc721bb454871937f2a01d47d1fb3303
James Mitchell <james.mitchell81@live.co.uk>
2015-03-13 22:36:51 +0000

    Created DatabaseConnection

---
+++ _database/DatabaseConnection.php
@@ -0,0 +1,46 @@
+<?php namespace Database;
+
+use PDO;
+
+class DatabaseConnection
+{
+  private $host;
+  private $db_name;
+  private $connection_str;
+  private $user;
+  private $password;
+  private $connection;
+  private $db;
+
+  public function __construct()
+  {
+    // $this->host = "194.81.104.22";
+    // $this->db_name = "db_eco";
+    // $this->connection_str = "mysql:host={$this->host};dbname={$this->db_name}";
+    // $this->user = "ecobabylon";
+    // $this->password = "group0402";
+    $this->host = "localhost";
+    $this->db_name = "db_eco";
+    $this->connection_str = "mysql:host={$this->host};dbname={$this->db_name}";
+    $this->user = "root";
+    $this->password = "donkey";
+
+ }
+
+  public function connect()
+  {
+    try {
+      $this->db = new PDO($this->connection_str, $this->user, $this->password);
+      $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      echo "Err!!!: {$e->getMessage()}";
+    }
+    return $this->db;
+  }
+
+  public function closeConnection()
+  {
+    $this->db = null;
+  }
+
+}
