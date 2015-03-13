fd6413f609dc7451bd4609ed5a857c0e23940bda
James Mitchell <james.mitchell81@live.co.uk>
2015-03-13 22:35:59 +0000

    Updated EventDAO

--- _dao/EventDAO.php
+++ _dao/EventDAO.php
@@ -1,15 +1,49 @@
 <?php namespace DAO
 
+use Models;
+use Database;
+
 class EventDAO
 {
+  private $db;
 
   public function getEventByID(id)
   {
-    // add params.
-    sql = "SELECT * FROM Events WHERE idEvents = {:id}";
+    $this->db = new DatabaseQuery();
+    $this->db->set('id', $id, PDO::PARAM_INT);
+    $this->db->select("SELECT * FROM Events WHERE idEvents = {:id}");
+    $data = $this->db->first();
+
+    $event = new Event();
+    $event->setTitle($data['Title']);
+    $event->setDescription($data['Description']);
+    $event->setDateTime($data['StartDateTime'], $data['EndDateTime']);
+
+    // get attendees...staff
+    // 'SELECT idStaff FROM StaffDiary WHERE idEvents = {:id}'
+
+    // get attendees...student
+    // 'SELECT idStudent FROM StudentDiary WHERE idEvents = {:id}'
+
+    return $event;
   }
 
+  public function createNewEvent(Event $event)
+  {
+    $this->db = new DatabaseQuery();
+    $this->db->set('title', $event->getTitle());
+    $this->db->set('description', $event->getDescription());
+    $this->db->set('start', $event->getStartDateTime());
+    $this->db->set('end', $event->getEndDateTime());
 
+    $SQL = 'INSERT INTO Events (Title, Description, StartDateTime, EndDateTime)
+            VALUES (:title, :description, :start, :end)';
 
+    // do insert.
+
+    // for each attendee.
+      // insert.
+
+  }
 
 }
