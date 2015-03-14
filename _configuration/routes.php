a539d85fc2042196c1d2905286cdb26f94482efa
James Mitchell <james.mitchell81@live.co.uk>
2015-03-14 21:35:57 +0000

    Added more Routes

--- _configuration/routes.php
+++ _configuration/routes.php
@@ -5,17 +5,27 @@ include 'util/Route.php';
 use Util\Route;
 
 // Diary.
-$routes['dairy/:yyyy'] = new Route('DiaryYearController', 'DiaryYearModel', 'DiaryYearView');
-$routes['dairy/:yyyy/:mm'] = new Route('DiaryMonthController', 'DiaryMonthModel', 'DiaryMonthView');
-$routes['dairy/:yyyy/:mm/:dd'] = new Route('DiaryDayController', 'DiaryDayModel', 'DiaryDayView');
+$routes['dairy/:yyyy'] = new Route('DiaryYearModel', 'DiaryYearView', 'DiaryYearController');
+$routes['dairy/:yyyy/:mm'] = new Route('DiaryMonthModel', 'DiaryMonthView', 'DiaryMonthController');
+$routes['dairy/:yyyy/:mm/:dd'] = new Route('DiaryDayModel', 'DiaryDayView', 'DiaryDayController');
 
 // Noticifications.
-
+$routes['notifications'] = new Route('NoticificationModel', 'NoticificationView', 'NoticificationController');
+// $routes['notifications/:id'] = new Route('NoticificationModel', 'NoticificationView', 'NoticificationController');
+// $routes['notifications/:subject'] = new Route('DiaryYearController', 'DiaryYearModel', 'DiaryYearView');
 
 // Personal details.
-
-
+$routes['personal-details'] = new Route('StudentDetailsModel', 'StudentDetailsView', 'StudentDetailsController');
 
 // Timetable.
+$routes['timetable'] = new Route('TimetableModel', 'TimetableView', 'TimetableController');
 
+// Modules
+$routes['modules'] = new Route('ModuleModel', 'ModuleView', 'ModuleController');
+// $routes['modules/:code'] = new Route('DiaryYearController', 'DiaryYearModel', 'DiaryYearView');
+
+// Assignments
+$routes['assignments'] = new Route('AssignmentsModel', 'AssignmentsView', 'AssignmentsController');
+// $routes['assignments/:id'] = new Route('DiaryYearController', 'DiaryYearModel', 'DiaryYearView');
+// $routes['assignments/:module'] = new Route('DiaryYearController', 'DiaryYearModel', 'DiaryYearView');
 
