005d26cf335a5831c0b17871eefb09a59360db77
James Mitchell <james.mitchell81@live.co.uk>
2015-03-14 17:56:19 +0000

    Created ViewBuffer to display views

---
+++ util/ViewBuffer.php
@@ -0,0 +1,39 @@
+<?php namespace Util;
+
+// object to array.
+// get object vars.
+
+class ViewBuffer
+{
+  private $view;
+  private $defaultViewPath = "_views/";
+  private $data = [];
+
+  public function __construct($view = '')
+  {
+    $this->view = $view;
+    return $this;
+  }
+
+  public function addData($data)
+  {
+    $this->data = $data;
+
+    // if is an object.
+    return $this;
+  }
+
+  public function buff()
+  {
+    extract($this->data);
+    ob_start();
+    include "{$this->defaultViewPath}{$this->view}";
+    ob_end_flush();
+    // @ob_end_clean();
+  }
+}
+
+
+
+
+?>
