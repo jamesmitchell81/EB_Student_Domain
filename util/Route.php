09063a2e87be0c79cd7fbe38cd1a25939ba7bd8a
James Mitchell <james.mitchell81@live.co.uk>
2015-03-14 17:56:48 +0000

    Added utility classes for Routing and Requests.

---
+++ util/Route.php
@@ -0,0 +1,17 @@
+<?php namespace Util;
+
+// https://r.je/mvc-php-front-controller.html
+
+class Route
+{
+  public $model;
+  public $view;
+  public $controller;
+
+  public function __construct($model, $view, $controller)
+  {
+    $this->model = $model;
+    $this->$view = $view;
+    $this->controller = $controller;
+  }
+}
---
+++ util/Router.php
@@ -0,0 +1,9 @@
+<?php namespace Util;
+
+// https://r.je/mvc-php-front-controller.html.
+
+class Router
+{
+  // get routes from config.
+  // return required route.
+}
