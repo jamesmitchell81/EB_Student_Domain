2d8a9ef0cbbac44c6a74caa46248f76f022d6fa5
James Mitchell <james.mitchell81@live.co.uk>
2015-03-15 18:55:53 +0000

    moved Router function from FrontController

--- _controllers/Controller.php
+++ _controllers/Controller.php
@@ -8,42 +8,7 @@ class Controller
 
   public function __construct()
   {
-    include('./_configuration/routes.php');
-
-    if ( !isset($_GET['data']) )
-    {
-      // redirect
-      exit(0);
-    } else {
-      $data = $_GET['data'];
-    }
-
-    $wildcards = [
-      ':yyyy' => '\d{4}',
-      ':mm'   => '\d{2}',
-      ':dd'   => '\d{2}'
-    ];
-
-    // if is set...
-    $this->routes = $routes;
-    // else $this->routes = [];
-
-    // convert to regex
-    foreach ($this->routes as $key => $value)
-    {
-      $str = $key;
-      $str = str_replace(":yyyy", '\d{4}', $str);
-      $str = str_replace(":mm",   '\d{2}', $str);
-      $str = str_replace(":dd",   '\d{2}', $str);
-
-      if( preg_match('#^' . $str . '$#', $data) )
-      {
-        $this->route = $this->routes[$key];
-      } else {
-        // throw out..exit...
-      }
-      // print $str;
-    }
+ 
 
     // $model = new $this->route->model;
     include("{$this->route->controller}.php");
--- _util/Router.php
+++ _util/Router.php
@@ -1,9 +1,49 @@
 <?php namespace Util;
 
 // https://r.je/mvc-php-front-controller.html.
+include('./_util/Input.php');
 
 class Router
 {
   // get routes from config.
   // return required route.
+  public function __construct()
+  {
+  	include('./_configuration/routes.php');
+
+    if ( !isset($_GET['data']) )
+    {
+      exit(0); // redirect...
+    } else {
+      $data = $_GET['data'];
+    }
+
+    $wildcards = [
+      ':username' => '\d{8}',
+      ':yyyy' => '\d{4}',
+      ':mm'   => '\d{2}',
+      ':dd'   => '\d{2}'
+    ];
+
+    // if is set...
+    $this->routes = $routes;
+    // else $this->routes = [];
+
+    // convert to regex
+    foreach ($this->routes as $key => $value)
+    {
+      $str = $key;
+      $str = str_replace(":yyyy", '\d{4}', $str);
+      $str = str_replace(":mm",   '\d{2}', $str);
+      $str = str_replace(":dd",   '\d{2}', $str);
+
+      if( preg_match('#^' . $str . '$#', $data) )
+      {
+        $this->route = $this->routes[$key];
+      } else {
+        // throw out..exit...
+      }
+      // print $str;
+    }
+  }
 }
