fad9ea91bc0f4292f43db7067633a507f255f934
James Mitchell <james.mitchell81@live.co.uk>
2015-03-15 23:36:56 +0000

    Used router in controller.

--- _controllers/Controller.php
+++ _controllers/Controller.php
@@ -1,23 +1,32 @@
 <?php namespace Controllers;
 
+include './_util/Input.php';
+include './_util/Router.php';
+
+use Util\Input;
+use Util\Router;
+
 // FrontController
 class Controller
 {
-  private $routes;
+  private $router;
   private $route;
 
   public function __construct()
   {
- 
+    $data = Input::get('data');
+
+    $this->router = new Router($data);
+    $this->route = $this->router->getRoute();
+    // include("{$this->route->model}.php");
+    include("{$this->route->controller}.php");
+    // include("{$this->route->view}.php");
 
     // $model = new $this->route->model;
-    include("{$this->route->controller}.php");
-
     $controller = new $this->route->controller; //new $this->route->controller($model);
-    // $view = new $this->route->view($model);
 
     // var_dump($this->route);
-    var_dump(explode('/', $data));
+    // var_dump(explode('/', $data));
   }
 
 }
