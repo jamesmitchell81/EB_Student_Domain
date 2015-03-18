27ae3dbf2e05dfdab26f9b04833d033a1942c21a
James Mitchell <james.mitchell81@live.co.uk>
2015-03-18 00:21:24 +0000

    mocked some times for daily template

--- _views/DiaryDailyView.php
+++ _views/DiaryDailyView.php
@@ -8,15 +8,22 @@ class DiaryDailyView implements View
 {
   private $viewPath = '_templates/diary-day.php';
   private $buffer;
+  private $model;
 
-  public function __construct()
+  public function __construct($model)
   {
-    echo static::class;
+    $this->model = $model;
   }
 
   public function display()
   {
     $this->buffer = new ViewBuffer($this->viewPath);
+
+    $data["today"] = $this->model->getDate();
+    $data["hours"] = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
+                      "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"];
+
+    $this->buffer->addData($data);
 
     $this->buffer->buff();
   }
