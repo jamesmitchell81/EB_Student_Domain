de9d0e89047ab918dd797e109e65645316480642
James Mitchell <james.mitchell81@live.co.uk>
2015-03-18 00:21:42 +0000

    gets year

--- _views/DiaryYearView.php
+++ _views/DiaryYearView.php
@@ -8,15 +8,18 @@ class DiaryYearView implements View
 {
   private $buffer;
   private $viewPath = '_templates/diary-year.php';
+  private $model;
 
-  public function __construct()
+  public function __construct($model)
   {
-    echo static::class;
+    $this->model = $model;
   }
 
   public function display()
   {
     $this->buffer = new ViewBuffer($this->viewPath);
+    $data["entity"] = $this->model->getYear();
+    $this->buffer->addData($data);
     $this->buffer->buff();
   }
 }
