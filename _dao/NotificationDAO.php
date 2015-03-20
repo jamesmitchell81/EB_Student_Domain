9df80a790cd506889325b63ecb068fb7e33fd2a9
James Mitchell <james.mitchell81@live.co.uk>
2015-03-20 22:14:52 +0000

    Implemented getNotificationById

--- _dao/NotificationDAO.php
+++ _dao/NotificationDAO.php
@@ -13,9 +13,34 @@ use Database\DatabaseQuery;
 class NotificationDAO
 {
 
-  public function getNotificationById($id)
+  public function getNotificationById($id, $username)
   {
+    $this->db = new DatabaseQuery();
+    $this->db->set('id', $id, PDO::PARAM_INT);
+    $this->db->set('username', $username, PDO::PARAM_INT);
+    $this->db->select('SELECT n.Title AS Subject, n.Body, n.NotificationDate, s.Title, s.FirstName, s.Surname
+                       FROM Notifications n
+                       INNER JOIN Staff s ON s.idStaff = n.idStaff
+                       INNER JOIN NotificationReceivers r ON r.idNotifications = n.idNotifications
+                       WHERE r.idStudent = :username AND n.idNotifications = :id
+                       ORDER BY n.NotificationDate DESC');
+    $data = $this->db->first();
 
+    $notification = [];
+    extract($data);
+
+    $sender = new Person();
+    $sender->setTitle($Title);
+    $sender->setFirstName($FirstName);
+    $sender->setLastName($Surname);
+
+    $notifications[] = new Notification();
+    $notifications[0]->setSubject($Subject);
+    $notifications[0]->setBody($Body);
+    $notifications[0]->setSentDateTime($NotificationDate);
+    $notifications[0]->setSender($sender);
+
+    return $notifications;
   }
 
   public function getUserNotifications($username)
@@ -26,8 +51,10 @@ class NotificationDAO
                        FROM Notifications n
                        INNER JOIN Staff s ON s.idStaff = n.idStaff
                        INNER JOIN NotificationReceivers r ON r.idNotifications = n.idNotifications
-                       WHERE r.idStudent = :username');
+                       WHERE r.idStudent = :username ORDER BY n.NotificationDate DESC');
     $data = $this->db->all();
+
+    $notifications = [];
 
     foreach ($data as $index => $notice)
     {
