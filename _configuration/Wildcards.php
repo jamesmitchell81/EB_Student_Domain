9d8331ef5d578aae36dab24518e7243fd28d8b1a
James Mitchell <james.mitchell81@live.co.uk>
2015-03-20 20:08:26 +0000

    new :id wildcard

--- _configuration/Wildcards.php
+++ _configuration/Wildcards.php
@@ -19,6 +19,7 @@ class Wildcards
     static::$wildcards[':yyyy']     = '(\d{4})';        // 4 numbers
     static::$wildcards[':mm']       = '(\d{2})';        // 2 numbers
     static::$wildcards[':dd']       = '(\d{2})';        // 2 numbers
+    static::$wildcards[':id']      = '([0-9]*)';        // 2 numbers
     static::$wildcards[':action']   = '(^[a-zA-z]+$)';  // one or more letters only, no spaces.
   }
 
