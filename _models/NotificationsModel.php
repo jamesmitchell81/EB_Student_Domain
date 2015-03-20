6f7b6be8efa3f9b1533a8cf07c4f86854a0ba04e
James Mitchell <james.mitchell81@live.co.uk>
2015-03-20 20:11:07 +0000

    Implemented NotificationModel

--- _models/NotificationsModel.php
+++ _models/NotificationsModel.php
@@ -1,10 +1,36 @@
 <?php
 
+include '_entities/Notification.php';
+include '_entities/Person.php';
+include './_dao/NotificationDAO.php';
+
+use Util\Input;
+use Models\Entities\Notification;
+use Models\Entities\Person;
+use DAO\NotificationDAO;
+
 class NotificationsModel
 {
+  private $username = '20150001';
+  private $notifications;
+  private $args = [];
 
-  public function __construct()
+  public function __construct($args = [])
   {
-    echo static::class;
+    $this->args = $args;
   }
+
+  public function getNotifications()
+  {
+    $dao = new NotificationDAO();
+    if ( empty($args) )
+    {
+      return $dao->getUserNotifications($this->username);
+    } else {
+      $id = array_shift($args);
+      return $dao->getNotificationById();
+    }
+  }
+
+
 }
