6b7617d39c76099155de860d3a24ef61dbda9212
James Mitchell <james.mitchell81@live.co.uk>
2015-03-22 12:48:34 +0000

    Updated notification controller to call action on model

--- _controllers/NotificationsController.php
+++ _controllers/NotificationsController.php
@@ -10,6 +10,14 @@ class NotificationsController
     $this->model = $model;
     $this->view = $view;
 
+    $action = $this->model->getAction();
+    // http://php.net/manual/en/function.method-exists.php
+    // http://stackoverflow.com/questions/19537423/check-if-function-exists-in-class-before-calling-call-user-func
+    if ( method_exists($this->model, $action) && is_callable(array($this->model, $action)) )
+    {
+      call_user_func(array($this->model, $action));
+    }
+
     $this->view->display();
   }
 }
