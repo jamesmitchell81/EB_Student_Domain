afa6d7dde2590d13c818ab5ea1b0c4493063fa6f
James Mitchell <james.mitchell81@live.co.uk>
2015-03-22 12:48:14 +0000

    Updated front controller pass action to model

--- _controllers/Controller.php
+++ _controllers/Controller.php
@@ -26,7 +26,7 @@ class Controller
     include("{$this->route->controller}.php");
     include("./_views/{$this->route->view}.php");
 
-    $model = new $this->route->model($this->router->getArguments());
+    $model = new $this->route->model($this->router->getArguments(), $this->router->getAction());
     $view = new $this->route->view($model);
     $controller = new $this->route->controller($model, $view);
     // $view->display();
