0e89fc6c82f6f85484dfff394f60a8350cf882d8
James Mitchell <james.mitchell81@live.co.uk>
2015-03-22 22:03:32 +0000

    for generating timetable sessions.

---
+++ createTimetableSessions.php
@@ -0,0 +1,42 @@
+<?php
+
+include '_database/DatabaseQuery.php';
+
+use Database\DatabaseQuery;
+
+$dtStart = new DateTime();
+$dtEnd = new DateTime();
+
+$dtStart->setDate(2015, 1, 12);
+$dtEnd->setDate(2015, 4, 3);
+
+$db = new DatabaseQuery();
+
+// var_dump($dtStart->diff($dtEnd)); //->days);
+
+//from startDt to endDt.
+echo "INSERT INTO Session (Date, idTimetable) VALUES" . "\n";
+while ( $dtStart->diff($dtEnd)->days > 0 )
+{
+  // var_dump($dtStart->diff($dtEnd));
+
+  $weekday = $dtStart->format('D');
+  $db->set('weekday', $weekday, PDO::PARAM_STR);
+  $db->select('SELECT idTimetable FROM Timetable WHERE Weekday = :weekday');
+
+  $ids = $db->all();
+
+  foreach ($ids as $tt => $id) {
+    // var_dump($dtStart->format("Y-m-d"));
+    // var_dump($id['idTimetable']);
+    // $db->set('id', $id['idTimetable'], PDO::PARAM_INT);
+    // $db->set('dt', $dtStart->format("Y-m-d"), PDO::PARAM_STR);
+    echo "('" . ($dtStart->format("Y-m-d")) . "', " . $id['idTimetable'] . ")," . "\n";
+  }
+
+  $dtStart->modify("+1 day");
+}
+  // get weekday.
+  //SELECT idTimetable FROM Timetable WHERE Weekday = :weekday.
+
+
