31a8053452c7df4bd1771c1a5d8fe3e39f0606d1
James Mitchell <james.mitchell81@live.co.uk>
2015-03-22 12:49:46 +0000

    Updated Notification model to perform new actions

--- _models/NotificationsModel.php
+++ _models/NotificationsModel.php
@@ -3,8 +3,10 @@
 include '_entities/Notification.php';
 include '_entities/Person.php';
 include './_dao/NotificationDAO.php';
+include './_util/Redirect.php';
 
 use Util\Input;
+use Util\Redirect;
 use Models\Entities\Notification;
 use Models\Entities\Person;
 use DAO\NotificationDAO;
@@ -14,16 +16,24 @@ class NotificationsModel
   private $username;
   private $notifications;
   private $args = [];
+  private $action;
 
-  public function __construct($args = [])
+  public function __construct($args = [], $action = '')
   {
     $this->args = $args;
+    $this->action = $action;
     $this->username = Input::session('username');
+  }
+
+  public function getAction()
+  {
+    return $this->action;
   }
 
   public function getNotifications()
   {
     $dao = new NotificationDAO();
+
     if ( empty($this->args) )
     {
       return $dao->getUserNotifications($this->username);
@@ -33,5 +43,22 @@ class NotificationsModel
     }
   }
 
+  public function save()
+  {
 
+  }
+
+  public function delete()
+  {
+    if ( !empty($this->args) )
+    {
+      $id = array_shift($this->args);
+      $dao = new NotificationDAO();
+      $dao->deleteNotificationById($id, $this->username);
+      Redirect::to('modules');
+      // header("Location: {$newPath}");
+      // exit();
+      // redirect.
+    }
+  }
 }
