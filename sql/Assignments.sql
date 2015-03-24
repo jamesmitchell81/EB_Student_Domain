4ff474dad2438ab85eb2d59290984eee34b322f4
James Mitchell <james.mitchell81@live.co.uk>
2015-03-24 23:39:27 +0000

    Assignment SQL

--- sql/Assignments.sql
+++ sql/Assignments.sql
@@ -1,11 +1,24 @@
+USE db_eco;
+
 SELECT * FROM AssignmentSubmission;
 SELECT * FROM Assignment; 
 
--- Submitted not marked
+INSERT INTO AssignmentSubmission (idStudent, idAssignment, Grade, DateSubmitted)
+VALUES 
+(20150001, 2, 0.9, '2015-03-02'),
+(20150001, 3, NULL, '2015-04-02');
+
+-- Submitted not Marked
 SELECT a.* 
 FROM AssignmentSubmission a
 WHERE a.DateSubmitted IS NOT NULL
 AND a.Grade IS NULL;
+
+-- Submitted and Marked
+SELECT a.*
+FROM AssignmentSubmission a
+WHERE a.DateSubmitted IS NOT NULL
+AND a.Grade IS NOT NULL;
 
 -- Submitted 
 SELECT a.* 
@@ -19,15 +32,20 @@ INNER JOIN Assignment a ON a.idAssignment = s.idAssignment
 WHERE s.DateSubmitted IS NOT NULL
 AND s.DateSubmitted > a.DueDate;
 
+-- Overdue
+SELECT s.* 
+FROM AssignmentSubmission s
+INNER JOIN Assignment a ON a.idAssignment = s.idAssignment
+WHERE s.DateSubmitted IS NULL
+AND a.DueDate < NOW();
+
+
 SELECT a.idAssignment, a.idModuleCode, a.idLecturer, a.Title, a.ReleaseDate, a.DueDate, a.Weighting
 FROM Assignment a
 INNER JOIN ModuleStudents ms ON ms.idModuleCode = a.idModuleCode
 WHERE ms.idStudent = 20150001;
 
-INSERT INTO AssignmentSubmission (idStudent, idAssignment, Grade, DateSubmitted)
-VALUES 
-(20150001, 2, 0.9, '2015-03-02'),
-(20150001, 3, NULL, '2015-04-02');
+
 
 
 
