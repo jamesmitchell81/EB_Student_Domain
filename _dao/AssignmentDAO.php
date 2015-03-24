7df4551db8c740d6f68a85b425926c6592e9d036
James Mitchell <james.mitchell81@live.co.uk>
2015-03-24 01:48:16 +0000

    Added AssignmentDAO

---
+++ _dao/AssignmentDAO.php
@@ -0,0 +1,53 @@
+<?php namespace DAO;
+
+include_once './_database/DatabaseQuery.php';
+include_once './_models/_entities/Assignment.php';
+
+use PDO;
+use Database\DatabaseQuery;
+use Models\Entities\Assignment;
+
+class AssignmentDAO
+{
+    private $db;
+
+    public function getAssignmentsByModuleCode($code)
+    {
+        $this->db = new DatabaseQuery();
+        $this->db->set('code', $code, PDO::PARAM_STR);
+        $this->dt->select();
+
+        $data = $this->all();
+
+        $assignments = [];
+
+        foreach ($data as $index => $assignment) {
+            
+            extract($assignment);
+
+            $assignments[$index]->setTitle("");
+            $assignments[$index]->setTitle("");
+            $assignments[$index]->setTitle("");
+            $assignments[$index]->setTitle("");
+            // ...
+
+        }
+
+
+
+    }
+
+    public function getUSerAssignments($username)
+    {
+
+    }
+
+    public function getSubmittedAssignmentsByUser($username)
+    {
+
+    }
+
+
+
+
+}
