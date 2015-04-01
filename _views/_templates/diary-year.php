c989c956033d08a23d81a7515638bc95ef3d6340
James Mitchell <james.mitchell81@live.co.uk>
2015-04-01 22:07:29 +0100

    diary year started.

--- _views/_templates/diary-year.php
+++ _views/_templates/diary-year.php
@@ -1,61 +1,5 @@
-<?php include "head.php"; ?>
-<?php include "logo-column.php"; ?>
-<div class="col-2">
-<?php include "header-nav.php"; ?>
-  <article id="content">
-    <div class="wrap" id="content-header">
 
-      <h2><span class='page-action'>View</span><span class='page-entity'><?= "{$this->data['entity']}" ?></span></h2>
-    </div>
 
-    <div class="wrap" id="content-workspace">
-
-      <?php
-        include './_util/Month.php';
-
-        use Util\Month;
-
-        $dt = $this->data['start'];
-
-        $test = new DateTime();
-        $test->setDate(2015, 1, 2);
-
-        $oMonth = new Month($test);
-        $aMonth = $oMonth->getWeeks();
-
-        $weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-        $week = 0;
-        $m = $dt->format('F');
-
-        while ( $dt->format('Y') == '2015')
-        {
-          $week = 0;
-          $month = [];
-          $month[$week] = [];
-          while ( $dt->format('F') == $m )
-          {
-            $weekday = $dt->format('D');
-            $month[$week][$weekday] = newDay($dt);
-            if ( $dt->format('D') == 'Sun' ) $week++;
-            $dt->modify('+1 day');
-          }
-          $year[$m] = $month;
-          $m = $dt->format('F');
-        }
-
-        function newDay($dt)
-        {
-          return [
-            'date'    => $dt->format('d'),
-            'weekday' => $dt->format('D'),
-            'href'    => $dt->format('Y/m/d')
-          ];
-        }
-
-      ?>
-
-  <?php foreach ($year as $monthName => $monthWeeks) : ?>
   <table class="diary-year-month">
     <col class="diary-year-mon" />
     <col class="diary-year-tue" />
@@ -66,35 +10,20 @@
     <col class="diary-year-sun" />
     <thead>
       <tr>
-        <th colspan="7" class="diary-year-month-label"><?= "$monthName"; ?></th>
+        <th colspan="7" class="diary-year-month-label"><?= "$month"; ?></th>
       </tr>
       <tr>
-        <?php foreach ($weekdays as $day) : ?>
-          <th class="diary-year-day"><?= $day; ?></th>
-        <?php endforeach; ?>
+          <th class="diary-year-day"></th>
       </tr>
     </thead>
     <tbody>
-    <?php for ( $week = 0; $week < count($monthWeeks); $week++ ) : ?>
       <tr>
-      <?php foreach ($weekdays as $day) : ?>
-      <?php if ( array_key_exists($day, $monthWeeks[$week]) ) : ?>
         <td>
-          <a href='<?= "{$monthWeeks[$week][$day]['href']}"; ?>'>
-            <?= "{$monthWeeks[$week][$day]['date']}"; ?>
+          <a href=''>
+
           </a>
         </td>
-      <?php else : ?>
-        <td></td>
-      <?php endif; ?>
-      <?php endforeach; ?>
       </tr>
-     <?php endfor; ?>
     </tbody>
   </table>
-<?php endforeach; ?>
 
-  </div><!-- end wrap -->
-</article>
-
-</div>
--- sql/events.sql
+++ sql/events.sql
@@ -4,14 +4,17 @@ INSERT INTO Events (Title, Description, StartDateTime, EndDateTime, Type)
 VALUES 
 ("Personal tutor meeting", "Meeting to discuss current progress", '2015-02-02 10:00:00', '2015-02-02 11:00:00', "Tutorials");
 
-INSERT INTO Events (Title, Description, StartDateTime, EndDateTime, Type)
+INSERT INTO Events (Title, StartDateTime, EndDateTime, Type)
 VALUES 
-("Pick up new Laptop", "Meeting to discuss current progress", '2015-02-02 11:00:00', '2015-02-02 11:30:00', "Personal");
+("Pick up new Laptop", '2015-02-02 11:00:00', '2015-02-02 11:30:00', "Personal");
 
 SELECT * FROM Events;
 
 INSERT INTO StudentDiary(idStudent, idEvents)
 VALUES (20150001, 1);
+
+INSERT INTO StudentDiary(idStudent, idEvents)
+VALUES (20150001, 2);
 
 
 SELECT e.Title, e.Description, e.StartDateTime, e.EndDateTime, e.Type
@@ -20,4 +23,7 @@ INNER JOIN StudentDiary s ON s.idEvents = e.idEvents
 WHERE s.idStudent = 20150001
 AND date(e.StartDateTime) = '2015-02-02';
 
-SELECT 
+SELECT DISTINCT e.Type 
+FROM Events e
+INNER JOIN StudentDiary s ON s.idEvents = e.idEvents
+WHERE s.idStudent = 20150001;
